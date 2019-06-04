bc8edf5603160f773d353aaee2b06978360d83b2
Alvin <lcndance@hotmail.com>
2019-06-04 18:01:42 +0800

    修改timer

--- example/2-7-2-timer.js
+++ example/2-7-2-timer.js
@@ -17,6 +17,22 @@
 // console.log('waiting on timer1');  //第一個出現的訊息
 
 // setTimeout(timer => {
-//     clearTimeout(timer);  //取消指定的 setTimeout
+//     clearTimeout(timer);  //清除指定的 setTimeout
 //     console.log('cleared timer');
 // }, 3000, timer1);
+
+
+// setInterval 持續每隔一段時間，就呼叫 callback
+// clearInterval 清除 setInterval
+// 每隔 3 秒顯示 Hello Alvin
+// let interval = setInterval(name => {
+//     console.log(`Hello ${name}`);
+// }, 3000, 'Alvin');
+
+// // 30秒後，清除 setInterval
+// setTimeout(interval => {
+//     clearInterval(interval);  //清除 setInterval
+//     console.log('cleared Interval');
+// }, 30000, interval);
+
+// console.log('waiting on first interval...');
